faa62a4b59515a40e8caf7f0db51f376de96a9e1
Bojan Bunić <bojan@bunic.info>
2025-11-17 10:51:38 +0100

    add publishing message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,6 +20,19 @@ func main() {
 	}
 	defer rmq.Close()
 	fmt.Println("Peril game server connected to RabbitMQ!")
+	ch, err := rmq.Channel()
+	if err != nil {
+		log.Fatalf("could not create a channel: %v", err)
+	}
+
+	if err := pubsub.PublishJSON(ch,
+		string(routing.ExchangePerilDirect),
+		string(routing.PauseKey),
+		routing.PlayingState{
+			IsPaused: true,
+		}); err != nil {
+		log.Fatalf("could not publish message: %v", err)
+	}
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
